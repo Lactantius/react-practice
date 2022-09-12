1c3a027c44e68394f15183469e25911631d12c3f
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-12 14:16:14 -0500

    Part 1 complete

---
+++ part-1/index.js
@@ -0,0 +1,18 @@
+const App = () => {
+    return (
+        <div>
+            <FirstComponent />
+            <NamedComponent name="Bob" />
+        </div>
+    )
+}
+
+const FirstComponent = () => {
+    return <h1>A component</h1>
+}
+
+const NamedComponent = (props) => {
+    return <p>My name is {props.name}.</p>
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
