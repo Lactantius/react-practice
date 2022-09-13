e66e5b65e2233580b0621f20359c4626b2140ac6
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-13 15:26:57 -0500

    Add (failing) test for left arrow

--- react-state-carousel-coins/carousel/src/Carousel.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -34,3 +34,27 @@ it("works when you click on the right arrow", function () {
     queryByAltText("Photo by Pratik Patel on Unsplash")
   ).toBeInTheDocument();
 });
+
+it("works when you click on the left arrow", function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the third
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Josh Post on Unsplash")
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the third image to show, but not the first
+  expect(
+    queryByAltText("Photo by Richard Pasquarella on Unsplash")
+  ).not.toBeInTheDocument();
+  expect(
+    queryByAltText("Photo by Josh Post on Unsplash")
+  ).toBeInTheDocument();
+});
